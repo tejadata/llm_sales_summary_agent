2af4e559ded149a49d9cf12351eba70bf3183a0a
Viswateja Rayapaneni <viswatejaster@gmail.com>
2025-02-09 23:04:57 +0530

    Update retrive_data.py

--- retrive_data.py
+++ retrive_data.py
@@ -8,14 +8,15 @@ def execute_sql_query(sql_query):
     try:
         with engine.connect() as connection:
             result = connection.execute(
-                text(sql_query)).fetchall()
-            print(result._metadata.keys)
+                text(sql_query))
+            print("---------", result.keys())
             if result:
-                print(result)
+                rows = result.fetchall()
                 columns = result.keys()
                 # Convert each row to a dictionary
-                rows = [dict(zip(columns, row)) for row in result]
-                return rows  # Return the list of dictionaries
+                rows_dict = [dict(zip(columns, row)) for row in rows]
+                print(rows_dict)
+                return rows_dict  # Return the list of dictionaries
             return None
     except Exception as e:
         return {"error": str(e)}
