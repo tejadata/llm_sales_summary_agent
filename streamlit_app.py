573157222ed0bd701adffe1b9df8e21988da97b6
Viswateja Rayapaneni <viswatejaster@gmail.com>
2025-02-11 14:15:23 +0530

    modifed REST endpoint

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # API Endpoint (Replace with your actual API URL)
-API_URL = "http://127.0.0.1:8000/summerize"
+API_URL = "https://llm-sales-summary-agent.onrender.com/summerize"
 
 # Set Streamlit page config
 st.set_page_config(page_title="💬 Sales Chatbot", layout="wide")
